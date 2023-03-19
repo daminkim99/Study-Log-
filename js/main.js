d7a7199b7e2c840c09b936b751bfa84e955f97a1
Damin (Elena) Kim <elenanana0903@gmail.com>
2023-03-19 17:46:43 -0300

    add the first form, and js component when you press submit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-console.log('hello world');
+document.getElementById('button').addEventListener('click', () => {
+    //saves the userpick in a variable userPick before the div gets wiped out 
+    let userPick = document.querySelector('input[name="usage"]:checked').value
+    //this is just test so can delete 
+    console.log(userPick)
+    //clears out the form 
+    document.getElementById('bor').remove()})
